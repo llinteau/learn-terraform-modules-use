004dbd412b147ccacb9732873d8ce83e3982867c
llinteau <llinteau12@gmail.com>
2023-10-12 06:41:25 -0400

    i never committed my org name change

--- terraform.tf
+++ terraform.tf
@@ -2,14 +2,14 @@
 # SPDX-License-Identifier: MPL-2.0
 
 terraform {
-  /* Uncomment this block to use Terraform Cloud for this tutorial
+
   cloud {
-    organization = "organization-name"
+    organization = "LYSETTE_ORG2"
     workspaces {
       name = "learn-terraform-module-use"
     }
   }
-  */
+
 
   required_providers {
     aws = {
